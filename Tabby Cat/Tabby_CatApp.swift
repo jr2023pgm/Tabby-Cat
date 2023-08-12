a598a59a8cc5f8896be7fbeb7134e1e66c9becbc
Jay Ram <jr2023pgm@gmail.com>
2023-08-12 17:28:16 +0400

    Initial Commit

---
+++ Tabby Cat/Tabby_CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_CatApp.swift
+//  Tabby Cat
+//
+//  Created by jr on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
